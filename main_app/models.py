0e205da8ff668af5988e0530d382c6573cb07523
janusshan <janusshan.p@gmail.com>
2022-02-11 13:14:28 -0500

    initial commit

---
+++ main_app/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+
+# Create your models here.
+class Donut(models.Model):
+    name = models.CharField(max_length=15)
+    description = models.CharField(max_length=200)
+    price = models.IntegerField()
+    special = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    donut_quantity = models.IntegerField()
+    donut = models.ManyToManyField(Donut)
+
+    def __str__(self):
+        pass
+
+class User(models.Model):
+    user_name = models.CharField(max_length=15)
+    password = models.CharField(max_length=15)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user_name
+
+class Review(models.Model):
+    CHOICES = [1,2,3,4,5]
+
+    content = models.CharField(max_length=200)
+    rating = models.IntegerField(
+        max_length=1,
+        choices = CHOICES,
+        default = CHOICES[4]
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    donut = models.ForeignKey(Donut, on_delete=models.CASCADE)
+
+class Orders(models.Model):
+    order_no = models.IntegerField()
+    delivery_date= models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Profile(models.Model):
+    email = models.CharField(max_length=40)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Delivery_Address(models.Model):
+    address = models.CharField(max_length=15)
+    apartment = models.CharField(max_length=15)
+    city = models.CharField(max_length=15)
+    country = models.CharField(max_length=15)
+    Province = models.CharField(max_length=15)
+    postal_code = models.CharField(max_length=7)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
