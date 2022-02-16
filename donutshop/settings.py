7ee8f123ca31ef5d6e184cd73cb670c7188e3fc5
janusshan <janusshan.p@gmail.com>
2022-02-16 16:20:29 -0500

    config deployment #1

--- donutshop/settings.py
+++ donutshop/settings.py
@@ -128,3 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/profile/'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
